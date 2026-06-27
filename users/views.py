d09003674392e90dfe55dc75121261d019179869
devbros595 <atejiri@outlook.com>
2026-06-27 03:20:01 +0100

    Adjust base.html css

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def sign_in_view(request):
+    return render(request, "sign_in.html")
+
+
+def sign_up_view(request):
+    return render(request, "sign_up.html")
